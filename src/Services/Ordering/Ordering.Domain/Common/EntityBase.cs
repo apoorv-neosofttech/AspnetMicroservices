7ea5d424781f1db220a180334280bdb301636e46
apoorv-rane2 <apoorv.rane@neosoftmail.com>
2021-12-02 15:19:51 +0530

    Added Ordering.Domain files

---
+++ src/Services/Ordering/Ordering.Domain/Common/EntityBase.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Ordering.Domain.Common
+{
+    public abstract class EntityBase
+    {
+        public int Id { get; set; }
+        public string CreadtedBy { get; set; }
+        public DateTime CreatedOn { get; set; }
+        public string UpdatedBy { get; set; }
+        public DateTime UpdatedOn { get; set; }
+    }
+}
